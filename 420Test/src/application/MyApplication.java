d4a323ecea5c963ae6a6b3ddb4f7dc8ab2717e22
Douglas <Douglas@172.20.209.233>
2019-03-05 16:27:27 -0500

    plzwork

---
+++ 420Test/src/application/MyApplication.java
@@ -0,0 +1,36 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+// Runs the programs
+public class MyApplication extends Application {
+    
+  
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // Read file fxml and draw interface.
+            Parent root = FXMLLoader.load(getClass().getResource("/MyScene.fxml"));
+            primaryStage.setTitle("SpeghettiForZoppetti UML Editor");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+            
+            
+         
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    
+    
+    
+}
---
+++ 420Test/src/application/MyController.java
@@ -0,0 +1,103 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.layout.Region;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.Cursor;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.RectangleBuilder;
+import javafx.stage.Stage;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.MenuItem;
+
+public class MyController implements Initializable {
+
+	@FXML
+	private AnchorPane root;
+	
+
+	@FXML
+	private Rectangle newRec;
+
+	@FXML
+	private AnchorPane nodeSpace;
+
+	@FXML
+	private AnchorPane properties;
+
+	@FXML
+	double orgSceneX, orgSceneY;
+
+	@FXML
+	double orgTranslateX, orgTranslateY;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+		// TODO (don't really need to do anything here).
+
+	}
+
+	public void createRectangle(ActionEvent event) {
+		newRec = new Rectangle(30, 40, 100, 150);
+
+		newRec.setFill(Color.WHITE);
+
+		newRec.setStroke(Color.BLACK);
+
+		newRec.setCursor(Cursor.HAND);
+		newRec.setOnMousePressed(rectangleOnMousePressedEventHandler);
+		newRec.setOnMouseDragged(rectangleOnMouseDraggedEventHandler);
+		nodeSpace.getChildren().add(newRec);
+         
+	}
+	
+	public void drag(MouseEvent event) {
+		Node n = (Node) event.getSource();
+		n.setTranslateX(n.getTranslateX() + event.getX());
+		n.setTranslateY(n.getTranslateY() + event.getY());
+	}
+
+	EventHandler<MouseEvent> rectangleOnMousePressedEventHandler = new EventHandler<MouseEvent>() {
+
+		@Override
+		public void handle(MouseEvent t) {
+			orgSceneX = t.getSceneX();
+			orgSceneY = t.getSceneY();
+			orgTranslateX = ((Rectangle) (t.getSource())).getTranslateX();
+			orgTranslateY = ((Rectangle) (t.getSource())).getTranslateY();
+		}
+	};
+
+	EventHandler<MouseEvent> rectangleOnMouseDraggedEventHandler = new EventHandler<MouseEvent>() {
+
+		@Override
+		public void handle(MouseEvent t) {
+			double offsetX = t.getSceneX() - orgSceneX;
+			double offsetY = t.getSceneY() - orgSceneY;
+			double newTranslateX = orgTranslateX + offsetX;
+			double newTranslateY = orgTranslateY + offsetY;
+
+			((Rectangle) (t.getSource())).setTranslateX(newTranslateX);
+			((Rectangle) (t.getSource())).setTranslateY(newTranslateY);
+		}
+
+	};
+	
+	@FXML
+	public void closeApp() 
+	{
+		Platform.exit();
+	}
+
+}
