3137a34bedc9bea0c65ea3261939cdbc3d5a25ab
zgwhelpl <zgwhelpl@gmail.com>
2019-03-21 13:38:13 -0400

    relationship.java includes knowledge of what objects they are connected to, its title, both ends multiplicity (0..*) isDashed() and both ends arrow types
    Contains both getters and setters for all
    
    This could definitely be re-written, but this will work for now.
    Specifically, the arrowtype should be rewritten when we get a chance.

--- 420Test/src/application/relationship.java
+++ 420Test/src/application/relationship.java
@@ -1,15 +1,33 @@
 package application;
 
+import java.util.*;
+
+/*enum arrowType
+{
+	NONE, V_ARROW, EMPTYTRIANGLE, FILLEDTRIANGLE, EMPTYDIAMOND, FILLEDDIAMOND, //Add more as we find fit
+	//1		2			3				4				5			6			Number Correspondent 
+};	/**/	
+
 public class relationship {
+
+	static class arrow {
+//		EnumSet<arrowType> type;
+		int type;
+	}
+	
 	private classBox End1;
 	private classBox End2;
 	private String title = "";
-	private String End1relationship = "*..*"; //Can anyone think of a better title for these?
-	private String End2relationship = "*..*"; // you know.. the 1..* and 0..1's and shit
+	private String multiplicity1 = ""; //Can anyone think of a better title for these?
+	private String multiplicity2 = ""; // you know.. the 1..* and 0..1's and shit
+	private boolean isDashed = false;
+	private arrow arrow1 = new arrow();
+	private arrow arrow2 = new arrow();
+	
+
 	
 	/*
-	 * Needed: 	-Line Type
-	 * 			-Arrow Type for both end1 and end 2
+	 * Needed: 		-Arrow Type for both end1 and end 2
 	 * 				-therefore, a set of types for both line and arrow with checks
 	 * 
 	 * Thoughts: 
@@ -19,11 +37,6 @@ public class relationship {
 	 * 			min and max and set the format appropriately rather than allow the user to 
 	 * 			put whatever shit they want in there. 
 	 */
-	
-	
-	
-	
-	
 	/*
 	 * Thoughts on this style in setting relationship ends, we could do it
 	 * in one click and drag, keeping tabs on the thing clicked on and the 
@@ -45,12 +58,32 @@ public class relationship {
 		return this.title;
 	}
 	
-	public String getEnd1rel(){
-		return this.End1relationship;
+	public String getMultiplicity1(){
+		return this.multiplicity1;
 	}
 	
-	public String getEnd2rel(){
-		return this.End2relationship;
+	public String getMultiplicity2(){
+		return this.multiplicity2;
+	}
+	
+	public boolean isDashed(){
+		return this.isDashed;
+	}
+	
+	/*public EnumSet<arrowType> getArrow1(){
+		return this.arrow1.type;
+	}
+	
+	public EnumSet<arrowType> getArrow2(){
+		return this.arrow2.type;
+	}/**/
+	
+	public int getArrow1(){
+		return this.arrow1.type;
+	}
+	
+	public int getArrow2(){
+		return this.arrow2.type;
 	}
 	
 	//-----setters
@@ -58,11 +91,48 @@ public class relationship {
 		this.title = s;
 	}
 	
-	public void setEnd1rel(String s){
-		this.End1relationship = s;
+	public void setMultiplicity1(String s){
+		this.multiplicity1 = s;
 	}
 	
-	public void setEnd2rel(String s){
-		this.End2relationship = s;
+	public void setMultiplicity2(String s){
+		this.multiplicity2 = s;
+	}
+	
+	public void dash(){
+		this.isDashed = true;
+	}
+	
+	public void undash(){
+		this.isDashed = false;
+	}
+	
+	public void toggleDash(){
+		this.isDashed = !isDashed;
+	}
+	
+	/*public void setArrow1(EnumSet<arrowType> type){
+		arrow1.type.
+	}/**/
+	
+	public int setArrow1(int t){
+		if( t >= 1 && t <=6 ){
+			arrow1.type = t;
+			return t;
+		} else {
+			System.out.println("setArrow1(): Bad Arrow Type!");
+			return 0;
+		}
+	}
+	
+	public int setArrow2(int t){
+		if( t >= 1 && t <=6 ){
+			arrow2.type = t;
+			return t;
+		} else {
+			System.out.println("setArrow2(): Bad Arrow Type!");
+			return 0;
+		}
 	}
 }
+
