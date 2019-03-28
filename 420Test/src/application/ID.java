bade99d4f1045a3240c7b447a04df5820c353b56
zgwhelpl <zgwhelpl@gmail.com>
2019-03-28 14:21:59 -0400

    ID.java includes an arrayList of classBoxes. As both the ID and classbox are added to their arrays upon creation, their respective indexes should match. When we delete objects, this will no longer be the case, so keep that in mind. ALSO included a function where you can pass in an ID and get that classBox

--- 420Test/src/application/ID.java
+++ 420Test/src/application/ID.java
@@ -6,15 +6,29 @@ public class ID {
 	
 	UUID uniqueID;
 	ArrayList<UUID> UUIDlist = new ArrayList<UUID>();
+	ArrayList<classBox> nodeList = new ArrayList<classBox>();
 	
-	public UUID ID(){
+	public UUID ID(classBox cB){
 		uniqueID = UUID.randomUUID();
 		UUIDlist.add(uniqueID);
+		nodeList.add(cB);
 		return uniqueID;
 	}
 	
 	public String toString(){
 		return this.uniqueID.toString();
+	}
+	
+	/*
+	 * This assumes that we aren't DELETING any nodes, therefore all
+	 * ID's and classboxes (assuming we only use classboxes) are in 
+	 * the same index with respect to each other.  
+	 * 
+	 * This may need tinkered in the future if changes are made
+	 */
+	public classBox getClassBox(UUID id){
+		int index = UUIDlist.indexOf(id);
+		return nodeList.get(index);
 	}
 	
 }
