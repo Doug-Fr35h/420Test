0d5fd721498afe02d24be420b6f701f9e2475e3e
zgwhelpl <zgwhelpl@gmail.com>
2019-04-04 12:15:51 -0400

    Fixes to assertEquls on Doubles

--- 420Test/src/application/JUnitOnClassBox.java
+++ 420Test/src/application/JUnitOnClassBox.java
@@ -14,12 +14,12 @@ public class JUnitOnClassBox {
 		assertEquals("Default Attributes should be       : -", Box1.getAttributes(), "-");
 		assertEquals("Default Opperations should be      : -", Box1.getOperations(), "-");
 		assertEquals("Default Extra Components should be : (nothing)", Box1.getExtraComponents(), "");
-		assertEquals("Default xLocation should be        : 20", Box1.getXLocation(), 20);
-		assertEquals("Default yLocation should be        : 20", Box1.getYLocation(), 20);
+		assertEquals("Default xLocation should be        : 20", Box1.getXLocation(), 20, 3);
+		assertEquals("Default yLocation should be        : 20", Box1.getYLocation(), 20, 3);
 		assertEquals("Default connections.size           : 1", Box1.getRelationshipCount(), 1);
 		assertEquals("Default connections[0] should be   : null ",Box1.getRelIndexOf(0), null);
-		assertEquals("Default width should be             : 100", Box1.getWidth(), 100);
-		assertEquals("Default height should be              : 130", Box1.getHeight(), 130);
+		assertEquals("Default width should be             : 100", Box1.getWidth(), 100, 3);
+		assertEquals("Default height should be              : 130", Box1.getHeight(), 130, 3);
 	}
 
     // fields used together with @Parameter must be public
