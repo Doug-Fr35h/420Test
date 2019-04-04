a45df66817bd07bde3ec128b205e9c9ac02345d9
zgwhelpl <zgwhelpl@gmail.com>
2019-04-04 11:10:58 -0400

    Skeleton Structure for JUnit tests

--- 420Test/src/application/JUnitOnClassBox.java
+++ 420Test/src/application/JUnitOnClassBox.java
@@ -1,8 +1,8 @@
 package application;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.*; 
 
-import org.junit.Test;
+import org.junit.*;
 
 public class JUnitOnClassBox {
 
@@ -11,4 +11,38 @@ public class JUnitOnClassBox {
 		fail("Not yet implemented");
 	}
 
+    // fields used together with @Parameter must be public
+/*    @Parameter(0)
+    public int m1;
+//    @Parameter(1)
+    public int m2;
+//    @Parameter(2)
+    public int result;/**/
+	
+	public classBox box1;
+	public classBox box2;
+	public relationship R1;
+	public relationship R2;
+
+    // creates the test data
+   /* @Parameters
+    public static Collection<Object[]> data() {
+        Object[][] data = new Object[][] { { 1 , 2, 2 }, { 5, 3, 15 }, { 121, 4, 484 } };
+        return Arrays.asList(data);
+    }/**/
+
+
+ /*  @Test
+    public void testMultiplyException() {
+        MyClass tester = new MyClass();
+        assertEquals("Result", result, tester.multiply(m1, m2));
+    }/**/
+
+
+    // class to be tested
+    class MyClass {
+/*        public int multiply(int i, int j) {
+            return i *j;
+        }/**/
+    }
 }
