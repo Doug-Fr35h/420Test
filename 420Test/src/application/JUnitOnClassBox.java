cbbafa61334c86916d398364e0e44adce5251d3c
zgwhelpl <zgwhelpl@gmail.com>
2019-04-04 12:41:44 -0400

    testing

--- 420Test/src/application/JUnitOnClassBox.java
+++ 420Test/src/application/JUnitOnClassBox.java
@@ -16,16 +16,30 @@ public class JUnitOnClassBox {
 	@Test
 	public void defaultValues() {
 		//classBox Box1 = new classBox();
-		assertEquals("Default Title Should be            : Title", Box1.getTitle(), "Title");
-		assertEquals("Default Attributes should be       : -", Box1.getAttributes(), "-");
-		assertEquals("Default Opperations should be      : -", Box1.getOperations(), "-");
-		assertEquals("Default Extra Components should be : (nothing)", Box1.getExtraComponents(), "");
-		assertEquals("Default xLocation should be        : 20", Box1.getXLocation(), 20, 3);
-		assertEquals("Default yLocation should be        : 20", Box1.getYLocation(), 20, 3);
-		assertEquals("Default connections.size           : 1", Box1.getRelationshipCount(), 1);
-		assertEquals("Default connections[0] should be   : null ",Box1.getRelIndexOf(0), null);
-		assertEquals("Default width should be             : 100", Box1.getWidth(), 100, 3);
-		assertEquals("Default height should be              : 130", Box1.getHeight(), 130, 3);
+		assertEquals("Default Title Should be				: Title", Box1.getTitle(), "Title");
+		assertEquals("Default Attributes should be			: -", Box1.getAttributes(), "-");
+		assertEquals("Default Opperations should be			: -", Box1.getOperations(), "-");
+		assertEquals("Default Extra Components should be	: (nothing)", Box1.getExtraComponents(), "");
+		assertEquals("Default xLocation should be			: 20", Box1.getXLocation(), 20, 3);
+		assertEquals("Default yLocation should be			: 20", Box1.getYLocation(), 20, 3);
+		assertEquals("Default connections.size				: 1", Box1.getRelationshipCount(), 1);
+		assertEquals("Default connections[0] should be		: null ",Box1.getRelIndexOf(0), null);
+		assertEquals("Default width should be				: 100", Box1.getWidth(), 100, 3);
+		assertEquals("Default height should be				: 130", Box1.getHeight(), 130, 3);
+	}
+	
+	@Test
+	public void testTitle() {
+		String newTitle = "Testy Title";
+		Box1.setTitle(newTitle);
+		assertEquals("New Title Should Be					:" + newTitle, Box1.getTitle(), newTitle);
+	}
+	
+	@Test
+	public void testTitle2() {
+		//String newTitle = "Testy Title";
+		//Box1.setTitle(newTitle);
+		assertEquals("New Title Should Be					:", Box1.getTitle(), "Title");
 	}
 	
 }
