fd02ae853f9eca6d9116ee12f3a8e8f1f0feafec
zgwhelpl <zgwhelpl@gmail.com>
2019-04-26 13:36:41 -0400

    Tidy

--- 420Test/src/application/ID.java
+++ 420Test/src/application/ID.java
@@ -8,9 +8,6 @@ public class ID {
 	public Map< UUID,classBox> classBoxIDmap = new HashMap< UUID,classBox>();
 	public Map< UUID,relationship> relationshipIDmap = new HashMap< UUID,relationship>();
 
-	/*
-	 * Constructor / assigns UUID
-	 */
 	/*public ID(){
 		uniqueID = UUID.randomUUID();
 		Class.forName(Thread.currentThread().getStackTrace()[2].getClassName())
@@ -58,47 +55,5 @@ public class ID {
 	public relationship getRelationship() {
 		return relationshipIDmap.get(this);
 	}
-
-	/*
-	 * Relay Functions: Why? Just to cover bases. 
-	 * 			there were a few instances where
-	 * 			scope and stuff got in the way bc
-	 * 			it was box1.id.getTitle or something
-	 * 			and a rewrite seemed odd. Should have
-	 * 			cited this instance.... 
-	 * The goal: Write in such a way that we don't 
-	 * 			even use these. However, some existing
-	 * 			code didn't like the recent change to
-	 * 			End1 and End2 class changing from 
-	 * 			classBox to ID. And it looks better 
-	 * 			in those files to leave as is instead of 
-	 * 			having head.getClassBox().getTitle or 
-	 * 			longer cases in there. It gets ugly. 
-	 * 
-	 * 			Stay Sharp my dudes
-	 * /
-				public String getTitle(){
-					return this.getClassBox().getTitle();
-				}
-			
-				public double getXLocation(){
-					return this.getClassBox().getXLocation();
-				}
-				
-				public double getYLocation(){
-					return this.getClassBox().getYLocation();
-				}
-			
-				public String getAttributes(){
-					return this.getClassBox().getAttributes();
-				}
-			
-				public String getExtraComponents(){
-					return this.getClassBox().getExtraComponents();
-				}
-			
-				public String getOperations(){
-					return this.getClassBox().getOperations();
-				}/**/
 	
 }
