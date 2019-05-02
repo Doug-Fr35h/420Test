c69fcca5a60ae549a96ea78cfd4dee2cf28b3010
zgwhelpl <zgwhelpl@gmail.com>
2019-05-02 14:05:17 -0400

    JavaDoc for ID.java complete

--- 420Test/src/application/ID.java
+++ 420Test/src/application/ID.java
@@ -3,9 +3,9 @@ package application;
 import java.util.*;
 
 /**
- * 
+ * ID class creates unique identifiers for every object and maps those objects
+ * using that id in a Map, and contains functions to use that map
  * @author zacharygarrettwhelpley
- *
  */
 public class ID {
 
@@ -25,7 +25,7 @@ public class ID {
 	}/**/
 	
 	/**
-	 * 
+	 * constructor for an ID on classBox
 	 * @param cB
 	 */
 	public ID(classBox cB) {
@@ -37,7 +37,7 @@ public class ID {
 	}
 	
 	/**
-	 * 
+	 * constructor for and ID on a relationship
 	 * @param R
 	 */
 	public ID(relationship R){
@@ -49,31 +49,28 @@ public class ID {
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * @return ID of an object
 	 */
 	public UUID getID(){
 		return this.uniqueID;
 	}
 	
 	/** 
-	 * 
+	 * @return the ID in string form
 	 */
 	public String toString() {
 		return this.uniqueID.toString();
 	}
 
 	/**
-	 * 
-	 * @return
+	 * @return classBox with this ID
 	 */
 	public classBox getClassBox() {
 		return classBoxIDmap.get(this);
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * @return relationship with this ID
 	 */
 	public relationship getRelationship() {
 		return relationshipIDmap.get(this);
